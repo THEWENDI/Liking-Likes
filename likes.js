989cfacc87dac93a1d90ad4fbf711cb91be64e14
Wendi Li <issacbbb@hotmaill.com>
2021-12-21 22:44:33 -0800

    initial commit

---
+++ likes.js
@@ -0,0 +1,23 @@
+var countneil = 9;
+var countneilelement = document.querySelector("#num-like-9");
+
+function add1neil(){
+    countneil++;
+    countneilelement.innerText = countneil + " like(s)";
+}
+
+var countnichole = 12;
+var countnicholeelement = document.querySelector("#num-like-12");
+
+function add1nichole(){
+    countnichole++;
+    countnicholeelement.innerText = countnichole + " like(s)";
+}
+
+var countjim = 9;
+var countjimelement = document.querySelector("#num3");
+
+function add1jim(){
+    countjim++;
+    countjimelement.innerText = countjim + " like(s)";
+}
